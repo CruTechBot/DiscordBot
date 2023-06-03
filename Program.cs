073409123252bdda17800dd0f227edb3eaa9f4be
GenOneDamo <GenOneDamo@gmail.com>
2023-06-03 16:29:15 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+    var _client = new DiscordSocketClient();
+
+    _client.Log += Log;
+
+     var token = File.ReadAllText("token.txt");
+
+    await _client.LoginAsync(TokenType.Bot, token);
+    await _client.StartAsync();
+
+    // Block this task until the program is closed.
+    await Task.Delay(-1);
+
+
+
+Task Log(LogMessage msg)
+{
+    Console.WriteLine(msg.ToString());
+    return Task.CompletedTask;
+}
+
